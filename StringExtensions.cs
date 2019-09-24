d676395a7fb87e518cea31e20d1cf92bcfea0a42
Jonah Haney <Jonah.Haney@murphyusa.com>
2019-09-24 16:07:29 -0500

    Add file "StringExtensions.cs" with string.Split overloads

---
+++ StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Rusted
+{
+    public static class StringExtensions
+    {
+        public static string[] Split(this string @this, char separator, int maxCount)
+            => @this.Split(new char[] { separator }, maxCount);
+
+        public static string[] Split(this string @this, char separator, int maxCount, StringSplitOptions options)
+            => @this.Split(new char[] { separator }, maxCount, options);
+
+        public static string[] Split(this string @this, string separator, int maxCount, StringSplitOptions options)
+            => @this.Split(new string[] { separator }, maxCount, options);
+    }
+}
