bba03baa3830b0f561b0de2d43947087bf8126ab
Jonah Haney <Jonah.Haney@murphyusa.com>
2019-09-25 17:42:09 -0500

    Initial Commit for Parsing.cs

--- Parsing.cs
+++ Parsing.cs
@@ -1,5 +1,7 @@
 ﻿
-using BigInteger = System.Numerics.BigInteger;
+using System;
+using System.Globalization;
+using System.Numerics;
 
 namespace Rusted
 {
@@ -8,40 +10,43 @@ namespace Rusted
         public static Option<bool> ParseBool(this string @this)
             => bool.TryParse(@this, out bool tmp) ? Option.Some(tmp) : Option.None<bool>();
 
-        public static Option<byte> ParseByte(this string @this)
-            => byte.TryParse(@this, out byte tmp) ? Option.Some(tmp) : Option.None<byte>();
+        public static Option<byte> ParseByte(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => byte.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out byte tmp) ? Option.Some(tmp) : Option.None<byte>();
 
-        public static Option<sbyte> ParseSByte(this string @this)
-            => sbyte.TryParse(@this, out sbyte tmp) ? Option.Some(tmp) : Option.None<sbyte>();
+        public static Option<sbyte> ParseSByte(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => sbyte.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out sbyte tmp) ? Option.Some(tmp) : Option.None<sbyte>();
 
-        public static Option<decimal> ParseDecimal(this string @this)
-            => decimal.TryParse(@this, out decimal tmp) ? Option.Some(tmp) : Option.None<decimal>();
+        public static Option<decimal> ParseDecimal(this string @this, NumberStyles style = NumberStyles.Float, IFormatProvider provider = null)
+            => decimal.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out decimal tmp) ? Option.Some(tmp) : Option.None<decimal>();
 
-        public static Option<double> ParseDouble(this string @this)
-            => double.TryParse(@this, out double tmp) ? Option.Some(tmp) : Option.None<double>();
+        public static Option<double> ParseDouble(this string @this, NumberStyles style = NumberStyles.Float, IFormatProvider provider = null)
+            => double.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out double tmp) ? Option.Some(tmp) : Option.None<double>();
 
-        public static Option<float> ParseFloat(this string @this)
-            => float.TryParse(@this, out float tmp) ? Option.Some(tmp) : Option.None<float>();
+        public static Option<float> ParseFloat(this string @this, NumberStyles style = NumberStyles.Float, IFormatProvider provider = null)
+            => float.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out float tmp) ? Option.Some(tmp) : Option.None<float>();
 
-        public static Option<int> ParseInt(this string @this)
-            => int.TryParse(@this, out int tmp) ? Option.Some(tmp) : Option.None<int>();
+        public static Option<int> ParseInt(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => int.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out int tmp) ? Option.Some(tmp) : Option.None<int>();
 
-        public static Option<uint> ParseUInt(this string @this)
-            => uint.TryParse(@this, out uint tmp) ? Option.Some(tmp) : Option.None<uint>();
+        public static Option<uint> ParseUInt(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => uint.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out uint tmp) ? Option.Some(tmp) : Option.None<uint>();
 
-        public static Option<long> ParseLong(this string @this)
-            => long.TryParse(@this, out long tmp) ? Option.Some(tmp) : Option.None<long>();
+        public static Option<long> ParseLong(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => long.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out long tmp) ? Option.Some(tmp) : Option.None<long>();
 
-        public static Option<ulong> ParseULong(this string @this)
-            => ulong.TryParse(@this, out ulong tmp) ? Option.Some(tmp) : Option.None<ulong>();
+        public static Option<ulong> ParseULong(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => ulong.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out ulong tmp) ? Option.Some(tmp) : Option.None<ulong>();
 
-        public static Option<short> ParseShort(this string @this)
-            => short.TryParse(@this, out short tmp) ? Option.Some(tmp) : Option.None<short>();
+        public static Option<short> ParseShort(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => short.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out short tmp) ? Option.Some(tmp) : Option.None<short>();
 
-        public static Option<ushort> ParsUShort(this string @this)
-            => ushort.TryParse(@this, out ushort tmp) ? Option.Some(tmp) : Option.None<ushort>();
+        public static Option<ushort> ParseUShort(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => ushort.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out ushort tmp) ? Option.Some(tmp) : Option.None<ushort>();
 
-        public static Option<BigInteger> ParseBigInteger(this string @this)
-            => BigInteger.TryParse(@this, out BigInteger tmp) ? Option.Some(tmp) : Option.None<BigInteger>();
+        public static Option<BigInteger> ParseBigInteger(this string @this, NumberStyles style = NumberStyles.Integer, IFormatProvider provider = null)
+            => BigInteger.TryParse(@this, style, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), out BigInteger tmp) ? Option.Some(tmp) : Option.None<BigInteger>();
+
+        public static Option<DateTime> ParseDateTime(this string @this, IFormatProvider provider = null, DateTimeStyles styles = DateTimeStyles.None)
+            => DateTime.TryParse(@this, Option.Wrap(provider).UnwrapOr(CultureInfo.InvariantCulture), styles, out DateTime tmp) ? Option.Some(tmp) : Option.None<DateTime>();
     }
 }
