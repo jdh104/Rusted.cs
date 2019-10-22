a6fdb3032abe4b21f68d24ce4847b1d23d9b9a4a
Jonah Haney <Jonah.Haney@murphyusa.com>
2019-10-22 14:26:27 -0500

    Rewrite Result class to use concrete Exception type as its error instead of generic definition

--- Result.cs
+++ Result.cs
@@ -4,53 +4,40 @@ namespace Rusted
 {
     public static class Result
     {
-        public static Result<T, Exception> Err<T>(string err) => new Result<T, Exception>(new Exception(err));
+        public static Result<T> Err<T>(string err)
+            => new Result<T>(new Exception(err));
 
-        public static Result<T, E> Err<T, E>(E err)
-            where E: Exception, new()
-        {
-            return new Result<T, E>(err);
-        }
+        public static Result<T> Err<T>(Exception err)
+            => new Result<T>(err);
 
-        public static Result<T, E> Ok<T, E>(T val) 
-            where E: Exception, new()
-        {
-            return new Result<T, E>(val);
-        }
+        public static Result<T> Ok<T>(T val)
+            => new Result<T>(val);
 
-        public static Result<T, E> Wrap<T, E>(T val)
-            where E: Exception, new()
+        public static Result<T> Wrap<T>(T val)
         {
             if (val == null)
             {
-                return new Result<T, E>(val);
+                return new Result<T>(new Exception());
             }
             else
             {
-                return new Result<T, E>(new E());
+                return new Result<T>(val);
             }
         }
 
-        public static bool Equals<E>(this Result<string, E> @this, Result<string, E> other, StringComparison stringComparison)
-            where E: Exception, new()
-        {
-            return (@this.IsErr() && other.IsErr()) || (@this.IsOk() && @this.wrapped.Equals(other.wrapped, stringComparison));
-        }
+        public static bool Equals(this Result<string> @this, Result<string> other, StringComparison stringComparison)
+            => (@this.IsErr() && other.IsErr()) || (@this.IsOk() && @this.wrapped.Equals(other.wrapped, stringComparison));
 
-        public static bool Equals<E>(this Result<string, E> @this, string other, StringComparison stringComparison)
-            where E : Exception, new()
-        {
-            return @this.IsOk() && @this.wrapped.Equals(other, stringComparison);
-        }
+        public static bool Equals(this Result<string> @this, string other, StringComparison stringComparison)
+            => @this.IsOk() && @this.wrapped.Equals(other, stringComparison);
     }
 
-    public class Result<T, E> : IEquatable<Result<T, E>>, IEquatable<T> where E: Exception, new()
+    public class Result<T> : IEquatable<Result<T>>, IEquatable<T>
     {
-        public static implicit operator Result<T, E>(T t) => Result.Wrap<T, E>(t);
-        public static implicit operator Result<T, E>(E e) => Result.Err<T, E>(e);
+        public static implicit operator Result<T>(T t) => Result.Wrap<T>(t);
 
         internal T wrapped;
-        internal E error;
+        internal Exception error;
         internal bool ok;
         
         internal Result(T val)
@@ -59,20 +46,14 @@ namespace Rusted
             this.wrapped = val;
         }
         
-        internal Result(E err)
+        internal Result(Exception err)
         {
             this.ok = false;
-            this.error = err ?? new E();
+            this.error = err ?? new Exception();
         }
 
-        public bool Equals(Result<T, E> other)
+        public bool Equals(Result<T> other)
             => this.ok && this.wrapped.Equals(other.wrapped);
-
-        public bool Equals<X>(Result<T, X> other)
-            where X : Exception, new()
-        {
-            return this.ok && this.wrapped.Equals(other.wrapped);
-        }
 
         public bool Equals(T other)
             => this.ok && this.wrapped.Equals(other);
@@ -81,32 +62,38 @@ namespace Rusted
         /// Returns a string that represents the current Result.
         /// </summary>
         /// <returns>A string that represents the current Result.</returns>
-        public override string ToString() => ok ? $"Ok({wrapped.ToString()})" : $"Err({error.Message})";
+        public override string ToString() 
+            => ok ? $"Ok({wrapped.ToString()})" : $"Err({error.Message})";
 
-        public Result<U, E> And<U>(Result<U, E> res) => ok ? res : new Result<U, E>(error);
+        public Result<U> And<U>(Result<U> res) 
+            => ok ? res : new Result<U>(error);
         
-        public Result<U, E> AndThen<U>(Func<Result<U, E>> op) => ok ? op() : new Result<U, E>(error);
+        public Result<U> AndThen<U>(Func<Result<U>> op) 
+            => ok ? op() : new Result<U>(error);
         
-        public Option<E> Err() => ok ? new Option<E>() : new Option<E>(error);
+        public Option<Exception> Err() 
+            => ok ? new Option<Exception>() : new Option<Exception>(error);
         
         public bool IsOk() => ok;
         
         public bool IsErr() => !ok;
         
-        public Result<U, E> Map<U>(Func<T, U> op) => ok ? new Result<U, E>(op(wrapped)) : new Result<U, E>(error);
+        public Result<U> Map<U>(Func<T, U> op) 
+            => ok ? new Result<U>(op(wrapped)) : new Result<U>(error);
         
-        public Result<U, E> MapOrElse<U>(Func<E, U> fallback, Func<T, U> map) => ok ? new Result<U, E>(map(wrapped)) : new Result<U, E>(fallback(error));
+        public Result<U> MapOrElse<U>(Func<Exception, U> fallback, Func<T, U> map) 
+            => ok ? new Result<U>(map(wrapped)) : new Result<U>(fallback(error));
         
-        public Result<T, E2> MapErr<E2>(Func<E, E2> op) 
-            where E2: Exception, new()
-        {
-            return ok ? new Result<T, E2>(wrapped) : new Result<T, E2>(op(error));
-        }
+        public Result<T> MapErr(Func<Exception, Exception> op) 
+            => ok ? new Result<T>(wrapped) : new Result<T>(op(error));
         
-        public Option<T> Ok() => ok ? new Option<T>(wrapped) : new Option<T>();
+        public Option<T> Ok() 
+            => ok ? new Option<T>(wrapped) : new Option<T>();
         
-        public T UnwrapOr(T optb) => ok ? wrapped : optb;
+        public T UnwrapOr(T optb) 
+            => ok ? wrapped : optb;
         
-        public T UnwrapOrElse(Func<T> op) => ok ? wrapped : op();
+        public T UnwrapOrElse(Func<T> op) 
+            => ok ? wrapped : op();
     }
 }
