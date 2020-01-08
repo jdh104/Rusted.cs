3fc46a54362d5932a80fcf5d0a555c54d23251a6
Jonah Haney <Jonah.Haney@murphyusa.com>
2020-01-08 13:11:44 -0600

    Add UnusableObject.cs

---
+++ UnusableObject.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Rusted
+{
+    [System.Obsolete("This is a compile-time check to prevent you from accidentally use the wrong version of a method")]
+    public class UnusableObject
+    {
+        public UnusableObject()
+        {
+            throw new System.Exception("You tried to use the UnusableObject class");
+        }
+    }
+
+    public class WrongMethodException : System.Exception { }
+}
